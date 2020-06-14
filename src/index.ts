2cca75fa2830e1ce7ee5e573a87f429402359541
david32145 <nascimento32145@gmail.com>
2020-06-14 17:50:18 -0300

    Config models

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Here")
+import Model from "./core/Model"
+
+class User {
+  private _id!: number
+  private _name!: string
+  private _email!: string
+  private _description!: string
+
+  public get id() : number {
+    return this._id
+  }
+
+  public get name() : string {
+    return this._name
+  }
+  
+  public get email() : string {
+    return this._email
+  }
+
+  public get description() : string {
+    return this._description
+  }
+}
